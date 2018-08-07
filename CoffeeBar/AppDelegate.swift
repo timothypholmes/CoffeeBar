de76e02ebb373fa5223db01b1041d5c985b4b61e
Timothyholmes11 <tpholmes7@gmail.com>
2018-08-07 14:57:54 -0500

    Initial Commit

---
+++ CoffeeBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CoffeeBar
+//
+//  Created by Tim Holmes on 8/7/18.
+//  Copyright © 2018 Tim Holmes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
